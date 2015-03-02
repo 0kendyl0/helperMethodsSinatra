5980d1b4c28426007bae7f30a3f1509d1ceb16e0
Kendyl <lemahie3@msu.edu>
2015-03-02 12:57:05 -0500

    cleanup for methods_practice_helpers.rb file

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -9,10 +9,6 @@ module MethodsPracticeHelpers
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>#{message}</div>)
-    # Generates the HTML for a Bootstrap alert (useful since it can be diffcult
-    # to remember all HTML needed to do it right). It should show the message in
-    # the "message" variable and should have a class appropriate for the "type"
-    # that is passed in, defaulting to 'info' if no type is passed in.
   end
 
   def current_date_and_time
